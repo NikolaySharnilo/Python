84a251d3025805aca0c851cfa84685d812055bf6
Nikolay Sharnilo <sharnilo.nikolay@gmail.com>
2022-07-30 12:53:09 +0500

    Task 2

--- Seminar_3/Task_2.py
+++ Seminar_3/Task_2.py
@@ -1,4 +1,15 @@
 
-
 # 2. Напишите программу, которая определить позицию второго вхождения строки в списке либо сообщит, что ее нет.
+# - список: ['qwe', 'asd', 'zxc', 'qwe', 'ertye'], ищем: "qwe", ответ: 3
 
+strList = ['qwe', 'asd', 'zxc', 'qwe', 'f', 'ertye']
+f = "qwe"
+score = 2
+for i in range(0, len(strList)):
+    if f in strList[i]:
+        score -= 1
+        if score == 0:
+            print(i)
+            exit()
+    if i == len(strList)-1:
+        print(-1)
